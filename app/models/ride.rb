7015d3617ae52b8764028a46716eb69b5e6265e0
trevismcgill <trevismcgill25@gmail.com>
2020-10-09 14:49:40 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,22 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        user = User.find_by_id(self.user_id)
+        attraction = Attraction.find_by_id(self.attraction_id)
+
+        if user.tickets >= attraction.tickets && user.height >= attraction.min_height
+            user.tickets = user.tickets - attraction.tickets
+            user.nausea = user.nausea + attraction.nausea_rating
+            user.happiness = user.happiness + attraction.happiness_rating
+            user.save
+        elsif user.tickets < attraction.tickets && user.height < attraction.min_height
+            "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
+        elsif user.tickets < attraction.tickets
+            "Sorry. You do not have enough tickets to ride the Roller Coaster."
+        elsif user.height < attraction.min_height
+            "Sorry. You are not tall enough to ride the Roller Coaster."
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def mood
+        if self.happiness > self.nausea
+            "happy"
+        else
+            "sad"
+        end
+    end
 end
